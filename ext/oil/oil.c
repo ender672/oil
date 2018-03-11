b01c7a1ddccb0e7dfd82fd47ee0214cf15ebc96b
Tim Elliott <tle@holymonkey.com>
2018-03-10 23:20:18 -0800

    oil.rb: use c-library cubic_scale_ratio() and fix_ratio()

--- ext/oil/oil.c
+++ ext/oil/oil.c
@@ -1,10 +1,39 @@
 #include <ruby.h>
+#include "resample.h"
+
+static VALUE rb_cubic_scale_ratio(VALUE self, VALUE src_dim, VALUE out_dim)
+{
+	int num, denom;
+	VALUE ret;
+	denom = cubic_scale_ratio(NUM2INT(src_dim), NUM2INT(out_dim), &num);
+	ret = rb_ary_new2(2);
+	rb_ary_push(ret, INT2FIX(num));
+	rb_ary_push(ret, INT2FIX(denom));
+	return ret;
+}
+
+static VALUE rb_fix_ratio(VALUE self, VALUE src_w, VALUE src_h, VALUE out_w, VALUE out_h)
+{
+	uint32_t out_width, out_height;
+	VALUE ret;
+	out_width = NUM2INT(out_w);
+	out_height = NUM2INT(out_h);
+	fix_ratio(NUM2INT(src_w), NUM2INT(src_h), &out_width, &out_height);
+	ret = rb_ary_new2(2);
+	rb_ary_push(ret, INT2FIX(out_width));
+	rb_ary_push(ret, INT2FIX(out_height));
+	return ret;
+}
 
 void Init_jpeg();
 void Init_png();
 
 void Init_oil()
 {
-   Init_jpeg();
-   Init_png();
+	VALUE mOil;
+	mOil = rb_const_get(rb_cObject, rb_intern("Oil"));
+	rb_define_singleton_method(mOil, "cubic_scale_ratio", rb_cubic_scale_ratio, 2);
+	rb_define_singleton_method(mOil, "fix_ratio", rb_fix_ratio, 4);
+	Init_jpeg();
+	Init_png();
 }
--- lib/oil.rb
+++ lib/oil.rb
@@ -27,42 +27,6 @@ module Oil
 
   private
 
-  def self.fix_ratio(sw, sh, boxw, boxh)
-    x = boxw / sw.to_f
-    y = boxh / sh.to_f
-
-    destw = boxw
-    desth = boxh
-
-    if x < y
-      desth = (sh * x).round
-    else
-      destw = (sw * y).round
-    end
-
-    if desth < 1
-      desth = 1
-    end
-
-    if destw < 1
-      destw = 1
-    end
-
-    return destw, desth
-  end
-
-  def self.cubic_scale_ratio(src_dim, out_dim)
-    tmp = out_dim * 8 * 3 / (src_dim * 2)
-    rest = out_dim * 8 * 3 % (src_dim * 2)
-    if (rest)
-      tmp += 1
-    end
-    if (tmp < 8)
-      return tmp, 8
-    end
-    return 1, 1
-  end
-
   def self.new_jpeg_reader(io, box_width, box_height)
     o = JPEGReader.new(io, [:COM, :APP1, :APP2])
 
@@ -72,13 +36,11 @@ module Oil
     end
 
     # JPEG Pre-scaling is equivalent to a box filter at an integer scale factor.
-    # We don't use this to scale down past 4x the target image size in order to
-    # get proper bicubic scaling in the final image.
-    scale_num, scale_denom = self.cubic_scale_ratio(o.image_width, box_width)
+    scale_num, scale_denom = Oil.cubic_scale_ratio(o.image_width, box_width)
     o.scale_num = scale_num
     o.scale_denom = scale_denom
 
-    destw, desth = self.fix_ratio(o.output_width, o.output_height, box_width, box_height)
+    destw, desth = Oil.fix_ratio(o.output_width, o.output_height, box_width, box_height)
     o.scale_width = destw
     o.scale_height = desth
 
